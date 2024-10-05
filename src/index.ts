2839d0997533e9491c2c85a3be83fe77bc350887
Eugene Marcotte <emarcotte@gmail.com>
2024-10-05 15:21:52 -0400

    more agro lint

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,30 @@
-import Phaser from 'phaser';
-
+import Phaser from "phaser";
+import RadmarsScreen from "./scenes/RadmarsScreen";
 
 export const SIZE = 800;
 export const WINDOW_WIDTH = 800;
 export const WINDOW_HEIGHT = 800;
 
 export const WINDOW_CENTER = {
-    x: WINDOW_WIDTH / 2,
-    y: WINDOW_HEIGHT / 2,
+  x: WINDOW_WIDTH / 2,
+  y: WINDOW_HEIGHT / 2,
 };
 
-const config: Phaser.Types.Core.GameConfig = {
-    type: Phaser.AUTO,
-    width: WINDOW_WIDTH,
-    height: WINDOW_HEIGHT,
-    pixelArt: true,
-    physics: {
-        default: 'arcade',
-    },
-	scene: [] as Phaser.Scene[],
+const config = {
+  type: Phaser.AUTO,
+  width: WINDOW_WIDTH,
+  height: WINDOW_HEIGHT,
+  pixelArt: true,
+  physics: {
+    default: "arcade",
+  },
+  scene: <Phaser.Scene[]>[],
 };
 const urlParams = new URLSearchParams(window.location.search);
-const devMode = urlParams.get('dev') === 'true' ? true : false;
+const devMode = urlParams.get("dev") === "true" ? true : false;
 
 if (!devMode) {
-    // config.scene.push(new RadmarsScreen());
+  config.scene.push(new RadmarsScreen());
 }
 
 new Phaser.Game(config);
---
+++ src/scenes/RadmarsScreen.ts
@@ -0,0 +1,105 @@
+import Phaser from "phaser";
+import { WINDOW_CENTER, WINDOW_HEIGHT } from "../";
+
+export default class RadmarsScreen extends Phaser.Scene {
+  constructor() {
+    super("RadmarsScreen");
+  }
+
+  preload(): void {
+    this.load.image("bg", "assets/intro/intro_bg.png");
+    this.load.image("mars", "assets/intro/intro_mars.png");
+
+    this.load.spritesheet("glasses", "assets/intro/intro_glasses.png", {
+      frameWidth: 72,
+      frameHeight: 12,
+      spacing: 1,
+    });
+    this.load.spritesheet("radmars", "assets/intro/intro_radmars.png", {
+      frameWidth: 96,
+      frameHeight: 14,
+    });
+
+    this.load.audio("radmarslogo", [
+      "assets/intro/radmarslogo.m4a",
+      "assets/intro/radmarslogo.ogg",
+    ]);
+  }
+
+  create(): void {
+    this.anims.create({
+      key: "glassesIdle",
+      frames: [{ key: "glasses", frame: 0 }],
+    });
+    this.anims.create({
+      key: "glassesFlash",
+      frames: this.anims.generateFrameNumbers("glasses", { start: 1 }),
+      frameRate: 10,
+    });
+    this.anims.create({
+      key: "textIdle",
+      frames: [{ key: "radmars", frame: 0 }],
+    });
+    this.anims.create({
+      key: "textFlash",
+      frames: this.anims.generateFrameNumbers("radmars", {}),
+      frameRate: 10,
+      repeat: 3,
+    });
+
+    this.sound.play("radmarslogo", { volume: 0.5 });
+
+    const cx = WINDOW_CENTER.x;
+    const cy = WINDOW_CENTER.y;
+
+    this.add.image(cx, cy, "bg").setScale(2);
+    const radmarsGlasses = this.add
+      .sprite(cx, WINDOW_HEIGHT / 6, "glasses")
+      .setScale(2);
+    radmarsGlasses.play("glassesIdle");
+    const radmarsText = this.add.sprite(cx, cy + 100, "mars").setScale(2);
+
+    this.tweens.add({
+      targets: radmarsGlasses,
+      y: cy,
+      ease: "Linear",
+      duration: 1600,
+      onComplete: () => {
+        radmarsGlasses.play("glassesFlash");
+        radmarsGlasses.chain(["glassesIdle"]);
+      },
+    });
+    radmarsGlasses.on(
+      Phaser.Animations.Events.ANIMATION_COMPLETE_KEY + "glassesFlash",
+      () => {
+        // I tried to use playAfterDelay instead, but the delay was inherited by the chained animation.
+        this.time.addEvent({
+          delay: 500,
+          callback: () => {
+            radmarsText.play("textFlash");
+            radmarsText.chain(["textIdle"]);
+          },
+        });
+      },
+    );
+    radmarsText.on(
+      Phaser.Animations.Events.ANIMATION_COMPLETE_KEY + "textFlash",
+      () => {
+        this.time.addEvent({
+          delay: 2000,
+          callback: () => {
+            this.cameras.main.fadeOut(1000);
+          },
+        });
+      },
+    );
+    this.cameras.main.once(
+      Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE,
+      () => {
+        this.scene.start("TileScreen");
+      },
+    );
+
+    this.cameras.main.fadeIn(1000);
+  }
+}

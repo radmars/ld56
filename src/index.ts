8502988a4b37dd65475c0faf9abae7c7f64a4edd
Eugene Marcotte <emarcotte@gmail.com>
2024-10-04 20:28:23 -0400

    init

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import Phaser from 'phaser';
+
+
+export const SIZE = 800;
+export const WINDOW_WIDTH = 800;
+export const WINDOW_HEIGHT = 800;
+
+export const WINDOW_CENTER = {
+    x: WINDOW_WIDTH / 2,
+    y: WINDOW_HEIGHT / 2,
+};
+
+const config: Phaser.Types.Core.GameConfig = {
+    type: Phaser.AUTO,
+    width: WINDOW_WIDTH,
+    height: WINDOW_HEIGHT,
+    pixelArt: true,
+    physics: {
+        default: 'arcade',
+    },
+	scene: [] as Phaser.Scene[],
+};
+
+
+const urlParams = new URLSearchParams(window.location.search);
+const devMode = urlParams.get('dev') === 'true' ? true : false;
+
+if (!devMode) {
+    // config.scene.push(new RadmarsScreen());
+}
+
+new Phaser.Game(config);
